98ea54eeb26aa1fb384962076e2b5328769c808b
Praveen Gupta <pvgupta24@gmail.com>
2020-02-18 10:47:37 +0530

    Adds Dockerfile and env support for viper

--- main.go
+++ main.go
@@ -64,6 +64,8 @@ func init() {
 
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	viper.AutomaticEnv()
+
 	err := viper.ReadInConfig()   // Find and read the config file
 	if err != nil {               // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s", err))
